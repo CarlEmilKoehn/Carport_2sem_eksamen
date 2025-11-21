96f263970ad5d645ffc94d2847580d81c71f5ed7
Tesselation93 <tessvanslyckspam@gmail.com>
2025-11-21 11:36:26 +0100

    Admin entitet oprettet med Lombok annotationer

--- carport/src/main/java/app/entities/Admin.java
+++ carport/src/main/java/app/entities/Admin.java
@@ -1,4 +1,18 @@
 package app.entities;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
 public class Admin {
+    private String adminEmail;
+    private String adminPassword;
+    private String adminFirstname;
+    private String adminLastname;
+
 }
