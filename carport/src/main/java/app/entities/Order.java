7b359fee2f6ea06d38fd22c1a07420388db564d5
olivermjmj <olivermjmj@outlook.dk>
2025-11-21 13:11:31 +0100

    I have made order and orderWithShed on topof that, i have fixed DomainModel, it now shows BigDecimal instead of BigInteger in OrderWithShed and Order.      The last thing i did was create put the database into database.sql and create data.sql and put our data inside.

--- carport/src/main/java/app/entities/Order.java
+++ carport/src/main/java/app/entities/Order.java
@@ -1,4 +1,22 @@
 package app.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+
 public class Order {
+
+    private int id;
+    private String email;
+    private String status;
+    private int widthMM;
+    private int heightMM;
+    private Timestamp createdAt;
+    private BigDecimal totalPrice;
+
 }
--- carport/src/main/java/app/entities/OrderWithShed.java
+++ carport/src/main/java/app/entities/OrderWithShed.java
@@ -1,4 +1,22 @@
 package app.entities;
 
-public class OrderWithShed {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+
+public class OrderWithShed extends Order{
+    public OrderWithShed(int id, String email, String status, int widthMM, int heightMM, Timestamp createdAt, BigDecimal totalPrice, int shedWidthMM, int shedLengthMM) {
+        super(id, email, status, widthMM, heightMM, createdAt, totalPrice);
+        this.shedWidthMM = shedWidthMM;
+        this.shedLengthMM = shedLengthMM;
+    }
+
+    private int shedWidthMM;
+    private int shedLengthMM;
+
 }
