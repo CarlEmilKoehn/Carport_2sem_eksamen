a6d2949dcb4c16b714f02acc9063b5743cdd4c9a
olivermjmj <olivermjmj@outlook.dk>
2025-11-21 14:06:32 +0100

    Created a test for getAllOrdersByEmail

--- carport/src/main/java/app/persistence/OrderMapper.java
+++ carport/src/main/java/app/persistence/OrderMapper.java
@@ -1,13 +1,62 @@
 package app.persistence;
 
 import app.entities.Order;
+import app.exceptions.DatabaseException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderMapper {
 
-    public List<Order> getAllOrders() {
+    public List<Order> getAllOrders() throws DatabaseException {
 
-        return null;
+        List<Order> orders = new ArrayList<>();
+
+        String sql = "";
+
+        try(Connection connection = ConnectionPool.getInstance().getConnection()) {
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+            }
+
+            return orders;
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Could not connect to DB: ", e.getMessage());
+        }
+    }
+
+    public List<Order> getAllOrdersByEmail(String email) throws DatabaseException {
+
+        List<Order> orders = new ArrayList<>();
+
+        String sql = "";
+
+        try(Connection connection = ConnectionPool.getInstance().getConnection()) {
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            stmt.setString(1, email);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+            }
+
+            return orders;
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Could not connect to DB: ", e.getMessage());
+        }
     }
 }
--- carport/src/test/java/app/persistence/OrderMapperTest.java
+++ carport/src/test/java/app/persistence/OrderMapperTest.java
@@ -2,6 +2,7 @@ package app.persistence;
 
 import app.entities.Order;
 import app.entities.OrderWithShed;
+import app.exceptions.DatabaseException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +27,7 @@ class OrderMapperTest {
     }
 
     @Test
-    void getAllOrders() {
+    void getAllOrders() throws DatabaseException {
 
         //--- given ---
         List<Order> expected = List.of(
@@ -42,5 +43,24 @@ class OrderMapperTest {
         //--- then ---
         assertEquals(expected, actual);
         assertEquals(expected.size(), actual.size());
+    }
+
+    @Test
+    void getAllOrdersByEmail() throws DatabaseException {
+
+        //--- given ---
+        List<Order> expected = List.of(
+                new Order(1, "a@a.com", "PENDING", 100, 200, Timestamp.valueOf("2024-01-01 10:00:00"), new BigDecimal("123.45")),
+                new Order(2, "d@d.com", "PAID", 300, 400, Timestamp.valueOf("2024-02-01 12:00:00"), new BigDecimal("456.78")),
+                new OrderWithShed(3, "c@c.com", "PENDING", 300, 400, Timestamp.valueOf("2024-02-01 12:00:00"), new BigDecimal("456.78"), 150, 250),
+                new OrderWithShed(4, "d@d.com", "PAID", 200, 100, Timestamp.valueOf("2024-02-01 12:00:00"), new BigDecimal("456.78"), 150, 250)
+        );
+
+        //--- when ---
+        List<Order> actual = orderMapper.getAllOrdersByEmail("d@d");
+
+        //--- then ---
+        assertEquals(expected, actual);
+        assertEquals(2, actual.size());
     }
 }
