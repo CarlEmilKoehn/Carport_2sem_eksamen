8dd271a63491360b061af65d4d990c9a23bbc3fc
olivermjmj <olivermjmj@outlook.dk>
2025-11-23 14:50:19 +0100

    Created an object to hold comments

--- carport/src/main/java/app/entities/Comment.java
+++ carport/src/main/java/app/entities/Comment.java
@@ -1,7 +1,11 @@
 package app.entities;
 
+import java.sql.Timestamp;
+
 public class Comment {
 
     private int id;
     private String note;
+    private Timestamp createdAt;
+    private String whichAdminMadeThisComment;
 }
