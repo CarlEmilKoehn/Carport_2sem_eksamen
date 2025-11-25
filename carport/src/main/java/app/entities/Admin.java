cc35acca8ec4b6ad81155eb0f48b467357551a1f
Tesselation93 <tessvanslyckspam@gmail.com>
2025-11-25 11:22:44 +0100

    Fjernet udnødvenige lombok annotationer

--- carport/src/main/java/app/entities/Admin.java
+++ carport/src/main/java/app/entities/Admin.java
@@ -2,23 +2,12 @@ package app.entities;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
-@ToString
-@Builder
 
 public class Admin {
     private String adminEmail;
     private String adminPassword;
     private String adminFirstname;
     private String adminLastname;
-
-    public Admin(String adminEmail, String adminFirstname, String adminLastname) {
-        this.adminEmail = adminEmail;
-        this.adminPassword = null;
-        this.adminFirstname = adminFirstname;
-        this.adminLastname = adminLastname;
-    }
 }
