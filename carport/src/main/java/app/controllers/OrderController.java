7646f3dc331f640ffb897093cdb3e74646a9ff91
CarlEmilKoehn <carledk@live.dk>
2025-12-02 09:48:05 +0100

    Initialised class

--- carport/src/main/java/app/controllers/OrderController.java
+++ carport/src/main/java/app/controllers/OrderController.java
@@ -1,13 +1,21 @@
 package app.controllers;
 
+import app.entities.Material;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+
 
 public class OrderController {
 
     public static void addRoutes(Javalin app) {
-
-
+        String svgText= "";
+        ctx.attribute("svg", svgText);
+        ctx.render("showOrder.html");
     }
-
 
 }
