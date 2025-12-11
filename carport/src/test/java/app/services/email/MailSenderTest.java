4f49d23192defc8757482213d8fbd4ccee2ae776
olivermjmj <olivermjmj@outlook.dk>
2025-12-11 15:30:18 +0100

    Preparing for writing tests

---
+++ carport/src/test/java/app/services/email/MailSenderTest.java
@@ -0,0 +1,23 @@
+package app.services.email;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailSenderTest {
+
+    private MailSender mailSender;
+    private EmailTemplateBuilder templates;
+
+    @Test
+    void sendEmail() {
+    }
+
+    @Test
+    void testSendEmail() {
+    }
+
+    @Test
+    void createAttachment() {
+    }
+}
