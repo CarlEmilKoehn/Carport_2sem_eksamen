1aa482e3c5762209bb8530bf0e375638e4fdcd49
olivermjmj <olivermjmj@outlook.dk>
2025-12-17 13:14:01 +0100

    Removed unused classes.

--- carport/src/main/java/app/exceptions/InputException.java
+++
@@ -1,8 +0,0 @@
-package app.exceptions;
-
-public class InputException extends ApplicationException {
-
-    public InputException(String userMessage, String systemMessage) {
-        super(userMessage, systemMessage);
-    }
-}
--- carport/src/main/java/app/exceptions/MailException.java
+++
@@ -1,8 +0,0 @@
-package app.exceptions;
-
-public class MailException extends ApplicationException {
-
-    public MailException(String userMessage, String systemMessage) {
-        super(userMessage, systemMessage);
-    }
-}
